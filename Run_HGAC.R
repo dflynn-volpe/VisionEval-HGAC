e31966d291864f0f7f15319b71e3f87090bc5a13
dflynn-volpe <daniel.flynn@dot.gov>
2023-05-22 08:04:18 -0400

    Attempts at TAZ setup

--- Run_HGAC.R
+++ Run_HGAC.R
@@ -1,11 +1,13 @@
 # Try out running HGAC scenarios
 
-tract_scenario_mod <- openModel('HGAC-RSPM-Tract')
-tract_scenario_mod$results()
+taz_scenario_mod <- openModel('HGAC-RSPM-TAZ')
+taz_scenario_mod <- openModel('HGAC-RSPM-TAZ_BaseOnly')
+
+taz_scenario_mod$results()
 
 start_time <- Sys.time()
 
-tract_scenario_mod$run()
+taz_scenario_mod$run()
 
 end_time <- Sys.time() - start_time
-print(paste(round(end_time, 2), attr(end_time, 'units'), 'to complete H-GAC tract-level scenarios'))
+print(paste(round(end_time, 2), attr(end_time, 'units'), 'to complete H-GAC taz-level scenarios'))
