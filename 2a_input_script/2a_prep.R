075f4921dd5ac343deda07456e8d0a6055d74cfb
Eric Englin <eric.englin@dot.gov>
2023-10-11 09:44:17 -0400

    fix years in 1a, start 2a

--- 2a_input_script/2a_prep.R
+++ 2a_input_script/2a_prep.R
@@ -1,13 +1,16 @@
 # Script to prepare inputs for HGAC VERSPM VisionEval model
-# This prepares inputs for Scenario 1a, where the number of dwelling units is altered to reflect changes in available housing on Galveston Island.
+# This prepares inputs for Scenario 2a, where the number of dwelling units is altered to reflect changes in employment growth in selected employment centers
 
-input <- read.csv('./1a_input_script/bzone_dwelling_units_2045.csv')
-base <- read.csv('./models/HGAC-RSPM-TAZ/inputs/bzone_dwelling_units.csv')
+
+input <- read.csv('bzone_dwelling_units_2045.csv')
+base <- read.csv('../models/HGAC-RSPM-TAZ/inputs/bzone_dwelling_units.csv')
 base <- base[base$Year==2019,]
-increase_file <- read.csv('./1a_input_script/Bzone1aGalReloTAZs.csv')
-decrease_file <- read.csv('./1a_input_script/GalvestonIsland1aDwellingUnits.csv')
-taz_groupings <- read.csv('./1a_input_script/TOE_TAZ-BZONE.csv')
-decrease_factor= 0.2
+
+# which bzones get increased? 
+increase_file <- read.csv('Bzone1aGalReloTAZs.csv')
+decrease_file <- read.csv('GalvestonIsland1aDwellingUnits.csv')
+taz_groupings <- read.csv('TOE_TAZ-BZONE.csv')
+decrease_factor= 0.05
 
 increase_list <- increase_file$NewTAZ_ID
 decrease_list <- decrease_file$NewTAZ_ID
@@ -44,8 +47,7 @@ input$GQDU[decrease_rows] <- (1-decrease_factor)*input$GQDU[decrease_rows]
 input$Bzone <- taz_groupings$Bzone
 
 #aggregates input into dataframe
-final <- aggregate(input[,c("SFDU","MFDU","GQDU")],by=list(Geo=taz_groupings$Bzone),sum)
-final$Year <- 2045
+final <- aggregate(input[,c("Year","SFDU","MFDU","GQDU")],by=list(Geo=taz_groupings$Bzone),sum)
 
 #Adding 3 sfdu to rows with 0 units
 final$SFDU[final$SFDU==0 & final$MFDU==0 & final$GQDU==0] <- 3
@@ -71,5 +73,5 @@ print(final)
 # We don't need to do that if we're just writing it to .csv (as.character is the default column processing)
 
 #exports final output to csv file
-if(!dir.exists('completed inputs')) { dir.create('completed inputs') }
-write.csv(final, "completed inputs/bzone_dwelling_units.csv", row.names=FALSE)
+if(!dir.exists('1a')) { dir.create('1a') }
+write.csv(final, "1a/bzone_dwelling_units.csv", row.names=FALSE)
